64d62660256c1bc152709dfec2cc439c223ef0f5
xonin-hush <xonindev@gmail.com>
2024-07-26 21:23:58 +0100

    Create gameBoard grids

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import {Ship} from "./modules/ship"
+import { createGrid } from "./modules/Gameboard"
---
+++ src/modules/Gameboard.js
@@ -0,0 +1,28 @@
+function createGridBoard() {}
+let CURRENT_COLOR_MODE = "default";
+
+const boardContainer1 = document.querySelector("#board1");
+const boardContainer2=document.querySelector("#board2")
+boardContainer1.classList.add("grid-container");
+boardContainer2.classList.add("grid-container");
+export function createGrid(itemNum = 20,container) {
+  container.style.gridTemplateColumns = `repeat(${itemNum},1fr)`;
+  container.style.gridTemplateRows = `repeat(${itemNum},1fr)`;
+  const cellSize = 40 / itemNum + "rem";
+  for (let i = 1; i < itemNum * itemNum + 1; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.setAttribute("id", "grid-item");
+    gridItem.classList.add("color-dark-blue");
+    gridItem.style.width = cellSize;
+    gridItem.style.height = cellSize;
+    gridItem.textContent = i;
+    gridItem.style.removeProperty("background-color");
+    gridItem.classList.remove("color-dark-blue");
+    gridItem.classList.add("color-sky-blue");
+    container.appendChild(gridItem);
+    // colorMode(gridItem);
+  }
+}
+
+createGrid(10,boardContainer1);
+createGrid(10,boardContainer2)
