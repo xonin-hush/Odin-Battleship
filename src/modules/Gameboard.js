6d4e2c4f1c2fab8a729dc3b349e6e0f44df3c891
xonin-hush <xonindev@gmail.com>
2024-07-28 06:19:12 +0100

    Add inputs for ships

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -5,10 +5,12 @@ const boardContainer1 = document.querySelector("#board1");
 const boardContainer2 = document.querySelector("#board2");
 boardContainer1.classList.add("grid-container");
 boardContainer2.classList.add("grid-container");
+
 export function createGrid(itemNum = 20, container) {
   container.style.gridTemplateColumns = `repeat(${itemNum},1fr)`;
   container.style.gridTemplateRows = `repeat(${itemNum},1fr)`;
   const cellSize = 40 / itemNum + "rem";
+  
   for (let i = 1; i < itemNum * itemNum + 1; i++) {
     const gridItem = document.createElement("div");
     gridItem.setAttribute("id", "grid-item");
