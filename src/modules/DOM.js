8aa67e8258578b3a58708dc91248460fe39249f5
xonin-hush <xonindev@gmail.com>
2024-08-07 12:39:29 +0100

    Locate squares on the corners if missed attack

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -5,13 +5,17 @@ const startButton = document.querySelector("#start");
 const stopButton = document.querySelector("#stop");
 const gameBoard1 = document.querySelector("#board1");
 let shipsExist = false;
+let ship1Position = "";
+let ship2Position = "";
+let ship3Position = "";
+let ship4Position = "";
 export function getShipPositions() {
   submit.addEventListener("click", (event) => {
     event.preventDefault();
-    let ship1Position = document.querySelector("#ship1");
-    let ship2Position = document.querySelector("#ship2");
-    let ship3Position = document.querySelector("#ship3");
-    let ship4Position = document.querySelector("#ship4");
+    ship1Position = document.querySelector("#ship1");
+    ship2Position = document.querySelector("#ship2");
+    ship3Position = document.querySelector("#ship3");
+    ship4Position = document.querySelector("#ship4");
     event.preventDefault();
     shipsExist = createShips(
       ship1Position,
@@ -35,12 +39,22 @@ function clickAttack() {
   gameBoard1.addEventListener("click", (e) => {
     if (e.target.getAttribute("id") == "grid-item1") {
       if (e.target.getAttribute("class") == "grid-item color-dark-blue") {
-        console.log(e)
+        console.log("hello",e.target.value);
         console.log("bingo");
+        missShot(e.target.value)
       }
     }
   });
 }
+
+function missShot(itemNumber){
+console.log(itemNumber+11)
+console.log(itemNumber+11-2)
+console.log(itemNumber-11)
+console.log(itemNumber-11+2)
+
+}
+
 clickAttack();
 startGame();
 getShipPositions();
